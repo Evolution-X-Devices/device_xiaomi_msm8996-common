02637293fdf5257a3d1622860d2ba0b21927d55f
Onelots <onelots@onelots.fr>
2025-05-06 15:50:21 +0200

    Revert "msm8996-common: Override fastboot-info.txt"
    This reverts commit fe8d4f86065677b8204a56b4a27e68026a415f65.
    Since we now use retrofit, this would only cause troubles
    while flashing the rom.

--- msm8996.mk
+++ msm8996.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2025 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -192,9 +192,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm-service.clearkey \
     libcrypto_shim.vendor
-
-# Fastboot
-TARGET_BOARD_FASTBOOT_INFO_FILE := $(LOCAL_PATH)/fastboot-info.txt
 
 # fastbootd
 PRODUCT_PACKAGES += \
