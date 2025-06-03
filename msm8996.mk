LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Set vendor init lib via soong config
Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

msm8996.mk
@@ -397,6 +397,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
 
+# Vendor init
+$(call soong_config_set,libinit,vendor_init_lib,//$(VENDOR_PATH):libinit.xiaomi_8996)
+
 # VNDK
 PRODUCT_PACKAGES += \
     libcutils-v29 \

Evolution-X-Devices/device_xiaomi_msm8996-common

msm8996.mk
@@ -410,6 +410,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
 
+# Vendor init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit.xiaomi_8996)
+
 # VNDK
 PRODUCT_PACKAGES += \
     libcutils-v29 \